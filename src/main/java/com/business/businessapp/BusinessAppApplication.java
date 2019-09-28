5eec0029a05fd5901d21374f3cdb37a5593d3929
ahmed-git <mansour-ahmed@live.fr>
2019-09-28 13:54:31 +0100

    test egit plugin

--- src/main/java/com/business/businessapp/BusinessAppApplication.java
+++ src/main/java/com/business/businessapp/BusinessAppApplication.java
@@ -8,7 +8,7 @@ public class BusinessAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessAppApplication.class, args);
-		System.out.print(true);
+		System.out.print(false);
 		
 	}
 
