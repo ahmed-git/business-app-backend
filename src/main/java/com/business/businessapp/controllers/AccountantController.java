4fce949b02e92730fd3de0ab05125504996fb045
ahmed-git <mansour-ahmed@live.fr>
2019-09-30 09:57:35 +0100

    add api for manager and accountant

--- src/main/java/com/business/businessapp/controllers/AccountantController.java
+++ src/main/java/com/business/businessapp/controllers/AccountantController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,11 @@ public class AccountantController {
 	@GetMapping("/all")
 	public List<Accountant> getAll() {
 		return accountantRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Accountant get(@PathVariable Long id) {
+		return accountantRepository.findById(id).get();
 	}
 	
 	@PostMapping("/update") 
--- src/main/java/com/business/businessapp/controllers/ManagerController.java
+++ src/main/java/com/business/businessapp/controllers/ManagerController.java
@@ -1,13 +1,18 @@
 package com.business.businessapp.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.business.businessapp.entities.Company;
 import com.business.businessapp.entities.Manager;
 import com.business.businessapp.repositories.ManagerRepository;
 
@@ -23,4 +28,19 @@ public class ManagerController {
 	public List<Manager> getAll() {
 		return managerRepository.findAll();
 	}
+	
+	@GetMapping("/{id}")
+	public Manager get(@PathVariable Long id) {
+		return managerRepository.findById(id).get();
+	}
+	
+	@PostMapping("/update")
+	public void save(@RequestBody Manager manager) {
+		Company company = new Company();
+		company.setId(manager.getCompany().getId());
+		manager.setCompany(company);
+		manager.setLastModified(LocalDate.now());
+		managerRepository.save(manager);
+	}
+	
 }
